8b49142a4eece4788416ed225c9c21839b1d6af4
ArturVilkicki <30698622+ArturVilkicki@users.noreply.github.com>
2018-02-05 17:36:04 +0200

    html
    html, css

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Arturo puslapis!!!</title>
+	<link href="https://fonts.googleapis.com/css?family=Encode+Sans+Expanded" rel="stylesheet">
+	<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="container-fluid">
+  		<div class="row">
+    		<div class="col">
+    			<div align="center">
+    				<img src="asmenine.jpg" width="500" height="600">
+    			</div>
+    		</div>
+    
+  		</div>
+  		<div class="row">
+    		<div class="col">
+    			<h2 align="center">Trumpas aprasymas:</h1>
+    				<div align="center">
+    					<i class="fas fa-address-book"></i>
+    				</div>
+    				
+    			<p class="tekstoSpalva">Esu Artur Vilkicki, man 25 metai!!!!</p>
+    		</div> 
+  		</div>
+  		<div class="row">
+    		<div class="col">
+    			<h2 align="center">Naujausi darbai:</h1>
+    			<div align="center">
+    				<a href="http://www.delfi.lt" target="_blank" align="center">Pirmas darbas</a>
+    			</div>
+    			<div align="center">
+    				<a href="http://www.facebook.com" target="_blank">Antras darbas</a>
+    			</div>
+    			<div align="center">
+    				<a href="http://www.kinozal.tv" target="_blank">Trecias darbas</a>
+    			</div>
+    			
+    		</div> 
+  		</div>
+  		<div class="row">
+    		<div class="col">
+    			<h2 align="center">Kontaktine informacija:</h1>
+    			<ul align="center">
+    				<li class="tekstoSpalva">Vardas Pavarde: Artur Vilkicki</li>
+    				<li class="tekstoSpalva">Kontaktinis telefonas: +370000000</li>
+    				<li class="tekstoSpalva"><a href="http://www.facebook.com" targer="_blank">nuoroda i facebook</li>
+    			</ul>
+    		</div> 
+  		</div>
+	</div>
+
+	
+	
+</body>
+</html>
